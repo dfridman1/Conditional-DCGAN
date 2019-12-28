61b7779bb2cc345d409d5679c61c8d82785dff2e
Denys Fridman <denys.fridman@pascal08.intra.deepsense.ai>
2019-12-28 07:46:32 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='cgan',
+    version='0.0.1',
+    description='Conditional GAN',
+    packages=find_packages(include=["cgan"]),
+    install_requires=[
+    ],
+)
